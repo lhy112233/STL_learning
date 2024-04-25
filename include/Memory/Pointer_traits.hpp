f3473c2ec3bfc938aed099a75f94da370c810d1d
yao <1746254723@qq.com>
2024-04-26 02:13:11 +0800

    暂时更新

--- include/Memory/Pointer_traits.hpp
+++ include/Memory/Pointer_traits.hpp
@@ -3,10 +3,22 @@
 #include <cstddef>
 #include <memory>
 #include <type_traits>
+std::pointer_traits<int> a;
 
 namespace hy {
 template <typename Ptr> struct pointer_traits {
 private:
+  template<typename Tp,typename = void>
+  struct element_type_impl;
+
+  template<typename Tp >
+  struct element_type_impl<Tp,typename Tp::element_type>{
+    using element_type = typename Tp::element_type;
+  };
+
+  template<>
+
+
   template <typename Tp, typename = void> struct difference_type_impl {
     using difference_type = std::ptrdiff_t;
   };
@@ -16,12 +28,23 @@ private:
     using difference_type = typename Ptr::difference_type;
   };
 
+  template <typename Tp,typename Up,typename = void>
+  struct rebind_type{};
+
+  template<typename Tp,typename Up>
+  struct rebind_type<Tp, Up,std::void_t<typename Tp::template rebind<Up>>>{
+    using rebind = typename Tp::template rebind<Up>;
+  };
+
+
+
 public:
   using pointer = Ptr;
-  // using element_type = ;
+  using element_type = typename element_type_impl<Ptr>::element_type;
   using difference_type = typename difference_type_impl<Ptr>::difference_type;
 
-
+  template<typename U>
+  using rebind = typename rebind_type<Ptr, U>::type;
 
   static pointer pointer_to(element_type &r) { return Ptr::pointer_to(r); }
 };
