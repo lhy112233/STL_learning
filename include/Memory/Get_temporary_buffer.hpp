25977b66caf572afea5d245305c0e97ada19b2e5
yao <1746254723@qq.com>
2024-07-11 00:29:45 +0800

    补充实现：return_temporary_buffer 和 get_temporary_buffer

---
+++ include/Memory/Get_temporary_buffer.hpp
@@ -0,0 +1,17 @@
+#ifndef HY_GET_TEMPORARY_BUFFER_HPP_
+#define HY_GET_TEMPORARY_BUFFER_HPP_
+#include <new>
+#include <utility>
+namespace hy {
+template <class T>
+[[deprecated]] std::pair<T *, std::ptrdiff_t>
+get_temporary_buffer(std::ptrdiff_t count) noexcept {
+  if (count <= 0) {
+    return {nullptr, 0};
+  } else {
+    return {static_cast<T *>(::operator new(count * sizeof(T))), count};
+  }
+}
+} // namespace hy
+
+#endif /// HY_GET_TEMPORARY_BUFFER_HPP_
