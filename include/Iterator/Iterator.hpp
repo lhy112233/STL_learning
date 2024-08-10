d8cd20c98b6ab1c0e6597c2e2898919eb60b3489
haiyaoli <1746254723abc@gmail.com>
2024-08-10 21:22:07 +0800

    Init

---
+++ include/Iterator/Iterator.hpp
@@ -0,0 +1,19 @@
+#ifndef HY_ITERATOR_HPP_
+#define HY_ITERATOR_HPP_
+#include <type_traits>
+
+namespace hy {
+
+
+
+}   //namespace hy
+
+
+
+
+
+
+
+
+
+#endif  //HY_ITERATOR_HPP_
---
+++ include/Iterator/Iterator_traits.hpp
@@ -0,0 +1,44 @@
+#ifndef HY_ITERATOR_TRAITS_HPP_
+#define HY_ITERATOR_TRAITS_HPP_
+
+namespace hy {
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+struct contiguous_iterator_tag : public random_access_iterator_tag {};
+
+template< class Iter >
+struct iterator_traits{
+    using difference_type = typename Iter::difference_type;
+    using value_type = typename Iter::value_type;
+    using pointer = typename Iter::pointer;
+    using reference = typename Iter::reference;
+    using iterator_category = typename Iter::iterator_category;
+
+    
+};
+
+
+
+
+}   //namespace hy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif  //HY_ITERATOR_TRAITS_HPP_
