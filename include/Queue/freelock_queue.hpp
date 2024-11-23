946c0312096f40d58b9d944ac403aa35940751f3
you_wsl_company <you@1746254723@qq.com>
2024-11-23 19:15:42 +0800

    SPMCQueue完成

---
+++ include/Queue/freelock_queue.hpp
@@ -0,0 +1,162 @@
+#ifndef HY_FREELOCK_QUEUE_HPP_
+#define HY_FREELOCK_QUEUE_HPP_
+#include <atomic>
+#include <concepts>
+#include <cstddef>
+#include <expected>
+#include <memory>
+#include <memory_resource>
+#include <new>
+#include <type_traits>
+#include <utility>
+
+namespace hy {
+template <typename T, std::size_t N, typename Alloc = std::allocator<T>>
+class FreelockQueue final : private Alloc {
+  static_assert(N > 1, "N cannot be 0");
+  static_assert(std::negation_v<std::is_reference<T>>,
+                "value_type is unable to same with value_type&&");
+
+ public:
+  using value_type = T;
+  using size_type = std::size_t;
+  using allocator_type = Alloc;
+  using allocator_traits = std::allocator_traits<Alloc>;
+
+  FreelockQueue(const FreelockQueue &) = delete;
+  FreelockQueue(FreelockQueue &&) = delete;
+  FreelockQueue &operator=(const FreelockQueue &) = delete;
+  FreelockQueue &operator=(FreelockQueue &&) = delete;
+
+  constexpr FreelockQueue()
+      : ring{allocator_traits::allocate(N)}, read_index_{0}, write_index_{0} {}
+
+  constexpr ~FreelockQueue() noexcept(
+      std::is_nothrow_destructible_v<value_type &>) {
+    if constexpr (std::is_trivially_destructible_v<value_type &>) {
+      auto read = read_index_.load(std::memory_order_acquire);
+      while (read != write_index_.load(std::memory_order_acquire)) {
+        allocator_traits::destroy(std::to_address(std::addressof(ring[read])));
+        read_index_.store((read_index_.load(std::memory_order_acquire) + 1) % N,
+                          std::memory_order_release);
+      }
+    }
+    allocator_traits::deallocate(ring, capacity());
+  }
+
+  constexpr void push(auto &&...args) noexcept(noexcept(value_type{
+      std::forward<decltype(args)>(args)...})) {
+    auto current_write = write_index_.load(std::memory_order_relaxed);
+
+    allocator_traits::construct(
+        *this, std::to_address(std::addressof(ring[current_write])),
+        std::forward<decltype(args)>(args)...);
+
+    // 延迟修改，万一构造抛异常了就不需要执行此操作
+    auto next_write = current_write + 1;
+    if (next_write == N) {
+      next_write = 0;
+    }
+    while (next_write == read_index_.load(std::memory_order_acquire)) {
+      read_index_.wait(next_write, std::memory_order_acquire);
+    }
+
+    write_index_.store(next_write, std::memory_order_release);
+    write_index_.notify_one();
+  }
+
+  constexpr void pop(value_type &value) noexcept(
+      std::disjunction_v<std::is_nothrow_move_assignable<value_type>,
+                         std::is_nothrow_copy_assignable<value_type>>) {
+    auto current_read = read_index_.load(std::memory_order_relaxed);
+    while (current_read == write_index_.load(std::memory_order_relaxed)) {
+      write_index_.wait(current_read, std::memory_order_acquire);
+    }
+
+    if constexpr (std::is_nothrow_move_assignable_v<value_type>) {
+      value = std::move(ring[current_read]);
+    } else {
+      value = ring[current_read];
+      allocator_traits::destroy(
+          *this, std::to_address(std::addressof(ring[current_read])));
+    }
+    ++current_read;
+    if (current_read == N) {
+      current_read = 0;
+    }
+    read_index_.store(current_read, std::memory_order_release);
+    read_index_.notify_one();
+  }
+
+  [[nodiscard]] constexpr bool try_push(auto &&...args) noexcept(
+      noexcept(value_type{std::forward<decltype(args)>(args)...})) {
+    auto current_write = write_index_.load(std::memory_order_relaxed);
+    auto next_write = current_write + 1;
+    if (next_write == N) {
+      next_write = 0;
+    }
+    if (next_write == read_index_.load(std::memory_order_acquire)) {
+      return false;
+    }
+
+    allocator_traits::construct(
+        *this, std::to_address(std::addressof(ring[current_write])),
+        std::forward<decltype(args)>(args)...);
+    write_index_.store(next_write, std::memory_order_release);
+    return true;
+  }
+
+  [[nodiscard]] constexpr bool try_pop(value_type &value) noexcept(
+      std::disjunction_v<std::is_nothrow_move_assignable<value_type>,
+                         std::is_nothrow_copy_assignable<value_type>>) {
+    auto current_read = read_index_.load(std::memory_order_relaxed);
+    if (current_read == write_index_.load(std::memory_order_acquire)) {
+      return false;
+    }
+    if constexpr (std::is_nothrow_assignable_v<value_type>) {
+      value = std::move(ring[current_read]);
+    } else {
+      value = ring[current_read]
+    }
+    if constexpr(std::negation_v<std::is_trivially_destructible<value_type>>) {
+      allocator_traits::destroy(*this, std::to_address(std::addressof(ring[current_read])));
+    }
+    ++current_read;
+    if (current_read == N) {
+      current_read = 0;
+    }
+    read_index_.notify_one();
+    return true;
+  }
+
+  constexpr std::size_t size() const noexcept {
+    int ret = read_index_.load(std::memory_order_acquire) -
+              write_index_.load(std::memory_order_acquire);
+    if (ret < 0) {
+      ret += N;
+    }
+    return ret;
+  }
+
+  constexpr bool empty() const noexcept {
+    return read_index_.load(std::memory_order_acquire) ==
+           write_index_.load(std::memory_order_acquire);
+  }
+
+  constexpr std::size_t capacity() const noexcept { return N - 1; }
+
+ private:
+  using AtmoicIndex = std::atomic_size_t;
+
+  char pad0[std::hardware_destructive_interference_size];
+  T *const ring;
+
+  alignas(std::hardware_destructive_interference_size) AtmoicIndex read_index_;
+  alignas(std::hardware_destructive_interference_size) AtmoicIndex write_index_;
+
+  char pad1[std::hardware_destructive_interference_size];
+};
+
+}  // namespace hy
+
+#endif  // HY_FREELOCK_QUEUE_HPP_
